4cf75b6a12f37e13b875d132e7fe946d6a7ac746
peng <peng@Mac-Pro.local>
2022-11-07 16:29:49 +0800

    update

--- modules.go
+++ modules.go
@@ -5,4 +5,6 @@ import (
 	_ "github.com/pengcu/caddy-trojan/app"
 	_ "github.com/pengcu/caddy-trojan/handler"
 	_ "github.com/pengcu/caddy-trojan/listener"
+	_ "github.com/pengcu/caddy-trojan/websocket"
+	_ "github.com/pengcu/caddy-trojan/grpc"
 )
