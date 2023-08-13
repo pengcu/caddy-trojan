5a6cfcc768d44929963e6513f79a32178e90764a
peng <peng@Mac-Pro.local>
2023-08-13 14:01:20 +0800

    update

--- modules.go
+++ modules.go
@@ -1,7 +1,6 @@
 package trojan
 
 import (
-	_ "github.com/pengcu/caddy-trojan/admin"
 	_ "github.com/pengcu/caddy-trojan/app"
 	_ "github.com/pengcu/caddy-trojan/handler"
 	_ "github.com/pengcu/caddy-trojan/listener"
